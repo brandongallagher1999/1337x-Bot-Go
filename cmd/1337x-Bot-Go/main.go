69faf20e47d9759b4f5ced3d1d961e654e9475b2
Herbert Yanes <rimgelair@gmail.com>
2022-03-24 16:01:29 -0400

    added profile flags to main

--- cmd/1337x-Bot-Go/main.go
+++ cmd/1337x-Bot-Go/main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
+	"runtime/pprof"
 
 	"github.com/brandongallagher199/1337x-Bot-Go/config"
 	"github.com/brandongallagher199/1337x-Bot-Go/internal/listener"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
+
 func main() {
+	flag.Parse()
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 	discordConfig, err := config.ReadDiscordConf()
 	if err != nil {
 		log.Fatal(err)
